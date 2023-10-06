5f6b4f7e1b75fec10a9f1d21a65922752226c4cd
krevetka87 <bonanast4sia@yandex.ru>
2023-10-06 16:52:12 +0300

    feat: add theme provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,16 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@/app/router';
 import { Provider } from 'react-redux';
 import store from '@/app/store';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from '@/app/theme';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </Provider>
   );
 }
