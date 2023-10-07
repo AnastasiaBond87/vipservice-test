4208f44e9ecb7d15a50d6a6cf3b4b6b5e3c3e02a
krevetka87 <bonanast4sia@yandex.ru>
2023-10-07 09:46:32 +0300

    feat: add some props to theme

--- src/app/theme/index.tsx
+++ src/app/theme/index.tsx
@@ -4,6 +4,23 @@ const theme = createTheme({
   palette: {
     primary: {
       main: '#5c87db',
+      dark: '#3e67b7',
+      light: '#5c87db',
+    },
+    secondary: {
+      main: '#b7bac1',
+      light: '#e0e1e3',
+    },
+    action: {
+      disabledBackground: '#b7bac1',
+      disabled: '#ffffff',
+    },
+    text: {
+      primary: '#5c5c5c',
+      secondary: '#b7bac1',
+    },
+    error: {
+      main: '#ef5350',
     },
   },
 });
