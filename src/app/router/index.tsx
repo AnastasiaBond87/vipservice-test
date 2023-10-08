59d24daefd505e292cb5ee3445d29268015d4467
krevetka87 <bonanast4sia@yandex.ru>
2023-10-08 16:52:09 +0300

    reafctor: add some changes to router

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import SearchPage from '@/pages/SearchPage';
 import CardPage from '@/pages/CardPage';
+import RootLayout from '@/layouts/RoutLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Navigate to="/avia" />} />
       <Route path="/avia" element={<SearchPage />} />
       <Route path="/avia/info" element={<CardPage />} />
       <Route path="*" element={<Navigate to="/avia" />} />
